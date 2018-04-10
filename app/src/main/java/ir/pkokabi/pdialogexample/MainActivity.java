0b05d1791f2a7d8586eac4c7de1ec1a3f7429c2c
p.kokabi <payam.kokabi@gmail.com>
2018-04-10 16:32:36 +0430

    init

--- app/src/main/java/ir/pkokabi/pdialogexample/MainActivity.java
+++ app/src/main/java/ir/pkokabi/pdialogexample/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Toast;
+
+import ir.pkokabi.pdialog.PDialog;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PDialog.FirstButtonCallBack {
 
     private Context context;
 
@@ -19,23 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
         AppCompatButton btn = findViewById(R.id.dialogBtn);
 
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new PDialog
-//                        .PDialogBuilder(context, "hi")
-//                        .titleColor(R.color.colorPrimary)
-//                        .isCancelable(false)
-//                        .firstButtonTitle("Ok")
-//                        .setFirstButtonCallBack(MainActivity.this)
-//                        .build();
-//            }
-//        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new PDialog
+                        .PDialogBuilder(context, "hi")
+                        .titleColor(R.color.colorPrimary)
+                        .isCancelable(false)
+                        .firstButtonTitle("Ok")
+                        .setFirstButtonCallBack(MainActivity.this)
+                        .build();
+            }
+        });
 
     }
-//
-//    @Override
-//    public void onFirstButtonClick() {
-//        Toast.makeText(this, "OK Clicked", Toast.LENGTH_SHORT).show();
-//    }
+
+    @Override
+    public void onFirstButtonClick() {
+        Toast.makeText(this, "OK Clicked", Toast.LENGTH_SHORT).show();
+    }
 }
